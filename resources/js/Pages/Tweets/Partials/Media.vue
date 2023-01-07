0b72794864f4cb777793e08182ab302a9beb805a
opmvpc <thibsix@outlook.be>
2023-01-07 04:09:17 +0100

    video upload

--- resources/js/Pages/Tweets/Partials/Media.vue
+++ resources/js/Pages/Tweets/Partials/Media.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { useForm } from "@inertiajs/inertia-vue3";
 import InputError from "@/Components/InputError.vue";
-import { TrashIcon } from "@heroicons/vue/24/outline";
+import { FilmIcon, TrashIcon } from "@heroicons/vue/24/outline";
 
 const props = defineProps({
     tweet: Object,
@@ -49,11 +49,19 @@ const deleteMedia = (mediumId) => {
     <div class="flex flex-col">
         <div class="grid grid-cols-4 gap-4 mt-2">
             <div
-                class="group h-20 relative shadow hover:shadow-md rounded cursor-pointer overflow-hidden"
+                class="w-20 h-20 group relative shadow hover:shadow-md rounded cursor-pointer overflow-hidden"
                 v-for="medium in tweet.media"
             >
+                <div
+                    v-if="medium.url.includes('.mp4')"
+                    class="absolute w-full h-20 aspect-square object-cover object-center group-hover:opacity-30 transition-all bg-black flex items-center justify-center"
+                    @click="deleteMedia(medium.id)"
+                >
+                    <FilmIcon class="h-10 w-10 text-gray-200" />
+                </div>
                 <img
-                    class="absolute w-full aspect-square group-hover:opacity-30 transition-all"
+                    v-else
+                    class="absolute w-full h-20 aspect-square object-cover object-center group-hover:opacity-30 transition-all"
                     @click="deleteMedia(medium.id)"
                     :src="medium.url"
                 />
@@ -67,8 +75,8 @@ const deleteMedia = (mediumId) => {
                 </div>
             </div>
             <div
-                v-show="!readonly"
-                class="bg-gray-100 text-xs flex items-center justify-center rounded shadow cursor-pointer p-2 text-center aspect-square hover:bg-gray-200 text-gray-800 hover:text-black active:bg-gray-300 hover:shadow-md active:text-black transition-all"
+                v-show="!readonly && tweet.media.length < 4"
+                class="h-20 w-20 bg-gray-100 text-xs flex items-center justify-center rounded shadow cursor-pointer p-2 text-center aspect-square hover:bg-gray-200 text-gray-800 hover:text-black active:bg-gray-300 hover:shadow-md active:text-black transition-all"
                 @click="$refs.fileInput.click()"
             >
                 <div>Add media</div>
